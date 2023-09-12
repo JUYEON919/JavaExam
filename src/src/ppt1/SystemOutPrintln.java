a38300db1471eb0be500115bcb31ac37f74f0be1
jyhan <wndus5357@naver.com>
2023-09-12 13:56:20 +0900

    시간체크

--- src/src/ppt1/SystemOutPrintln.java
+++ src/src/ppt1/SystemOutPrintln.java
@@ -5,7 +5,7 @@ public class SystemOutPrintln {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕!!");
+		System.out.println("Hello World!!");
 	}
 
 }
