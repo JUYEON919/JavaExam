049b17fb85cbd65316d0d5b7aea04c28d2a17e42
jyhan <wndus5357@naver.com>
2023-09-18 09:37:01 +0900

    익셉션

---
+++ src/ppt4/MainClass.java
@@ -0,0 +1,19 @@
+package ppt4;
+
+public class MainClass {
+    public static void main(String[] args) {
+    	
+    	int result = 0;
+    	
+    	try {
+    		result = 10/0;
+    		//오류발생
+    		
+    	}catch (Exception e) {
+    		System.out.println("catch : " + e);
+    	}finally {
+    		System.out.println("result : " + result);
+    		//db연결종료
+    	}
+    }
+}
---
+++ src/ppt4/MyCustomException.java
@@ -0,0 +1,8 @@
+package ppt4;
+
+public class MyCustomException extends Exception {
+	public MyCustomException(String message) {
+		super(message);
+		
+	}
+}
