0db72def957a03fd6a4fb3770e2d7e076528062d
jyhan <wndus5357@naver.com>
2023-09-19 09:39:35 +0900

    hashcode

--- src/ppt4/MainClass.java
+++ src/ppt4/MainClass.java
@@ -1,26 +1,39 @@
 package ppt4;
 
+import java.util.Arrays;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-		String strJava = "Hello";
+		int[] arrInt = {1,2,3,4,5};
+		int[] arrInt2 = new int[] {10,20,30,40,50};
 		
-		System.out.println("String before    :" + strJava.hashCode() + " : " + strJava);
-
-		strJava += " Java";
-		System.out.println("String after    :" + strJava.hashCode() + " : " + strJava);
+		int[] arrInt3 = new int[5];
+		int[] arrInt4 = new int[6];
+		
+		System.out.println(arrInt3.hashCode());
+		//대상 배열의 크기를 변경하지 않음
+		System.arraycopy(arrInt, 2, arrInt3, 1, 3);
+		System.out.println(arrInt3.hashCode());
 		System.out.println();
 		
-		StringBuffer strbJava = new StringBuffer("Hello");
-		System.out.println("StringBuffer before    :" + strbJava.hashCode() + " : " + strbJava);
-		System.out.println(strbJava.capacity());
+		//대상 배열의 크기가 변경됨
+		System.out.println(arrInt4.hashCode());
+		arrInt4 = Arrays.copyOf(arrInt2, 5);
+		System.out.println(arrInt4.hashCode());
 		
-		strbJava.append(" Java!!");
-		System.out.println("StringBuffer after    :" + strbJava.hashCode() + " : " + strbJava);
-		System.out.println(strbJava.capacity());
-	
+		for(int value:arrInt3) {
+			System.out.print(value + " ");
+		}
+		System.out.println();
+		
+		for(int value:arrInt4) {
+			System.out.print(value + " ");
+		}
+		System.out.println();
+		
+		
 	}
 	
 	
