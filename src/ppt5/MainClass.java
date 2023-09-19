9948047a448ae29794c4db09c65d3b42eaf47e8e
jyhan <wndus5357@naver.com>
2023-09-19 14:50:05 +0900

    Integer

--- src/ppt5/MainClass.java
+++ src/ppt5/MainClass.java
@@ -4,24 +4,34 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		Box<Integer> myBox1 = new Box<>(10);
-		Box<String> myBox2 = new Box<>("Java");
-		
 		System.out.println(myBox1.getValue());
+		System.out.println(Box.getCnt());
+
+		Box<String> myBox2 = new Box<>("Java");
 		System.out.println(myBox2.getValue());
+		//System.out.println(Box<T>.getCnt());
+		System.out.println(Box.getCnt());
+		
+		Box<Integer> myBox3 = new Box<>(20);
+		System.out.println(myBox3.getValue());
+		System.out.println(Box.getCnt());
 		
 	}
-
 }
 
 class Box<T>{
+	private static int cnt;
 	private T value;
 	public Box(T value) {
+		cnt++;
 		this.value = value;
 	}
 	public T getValue() {
 		return value;
 	}
-	
+	public static int getCnt() {
+		return cnt;
+	}
 }
 
 
